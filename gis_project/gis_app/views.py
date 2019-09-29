e45e76110b133af3181199e838d90321943313e8
Ivan Nab <ivan.nab@yandex.ru>
2019-09-29 16:04:57 +0300

    удаление perform_update

--- gis_project/gis_app/views.py
+++ gis_project/gis_app/views.py
@@ -43,6 +43,3 @@ class UserPositionViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-
-    def perform_update(self, serializer):
-        serializer.save(user=self.request.user)
