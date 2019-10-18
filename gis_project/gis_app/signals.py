6ca3dd7f8cf111c65ffdb9b671dc35814a51d4a4
Ivan Nab <ivan.nab@yandex.ru>
2019-10-18 14:18:26 +0300

    регистрация сигналов с помощью декораторов

--- gis_project/gis_app/signals.py
+++ gis_project/gis_app/signals.py
@@ -1,12 +1,20 @@
 import json
+from django.dispatch import receiver
+from django.db.models.signals import post_save, m2m_changed, post_delete
 
 
+@receiver(post_save,
+          sender="gis_app.UserPosition",
+          dispatch_uid="update_avg_coords")
 def update_avg_cords(sender, instance, **kwargs):
     avg_coords = instance.user.calculate_avg_coords()
     instance.user.avg_coords = json.dumps(avg_coords)
     instance.user.save()
 
 
+@receiver(m2m_changed,
+          sender="gis_app.UserVehicle",
+          dispatch_uid="update_user_vehicles_names_m2m")
 def update_user_vehicles_names_m2m(sender, instance, action, **kwargs):
     user_pk_set = kwargs["pk_set"]
     model = kwargs["model"]
@@ -18,6 +26,9 @@ def update_user_vehicles_names_m2m(sender, instance, action, **kwargs):
             user.save()
 
 
+@receiver([post_save, post_delete],
+          sender="gis_app.UserVehicle",
+          dispatch_uid="update_user_vehicles")
 def update_user_vehicles(sender, instance, **kwargs):
     vehicles_names = instance.user.get_vehicles_names()
     instance.user.vehicles = json.dumps(vehicles_names)
